2d3283b3e1e74e8661ff2bcdabf7ee38c4e99398
Aaron Bromma <36652096+bioresestance@users.noreply.github.com>
2025-12-01 18:08:34 +0000

    Add day 1 template

---
+++ day-01/src/part01.cpp
@@ -0,0 +1,8 @@
+/**
+ * Day-1 - Part 01
+ */
+#include "include.hpp"
+
+std::string handlePart1(const std::string_view input) {
+    return "";
+}
---
+++ day-01/src/part02.cpp
@@ -0,0 +1,8 @@
+/**
+ * Day-1 - Part 02
+ */
+#include "include.hpp"
+
+std::string handlePart2(const std::string_view input) {
+    return "";
+}
