07bb851a0e223d3cdd1f17c42a9379a24bde0454
Aaron Bromma <36652096+bioresestance@users.noreply.github.com>
2025-12-02 19:10:32 +0000

    Day02-part1 done

--- day-02/main.cpp
+++ day-02/main.cpp
@@ -14,9 +14,9 @@
 
 
 // Add the test input and output strings here to validate the solution
-const std::vector<std::string> testInputStrings = {""};
+const std::vector<std::string> testInputStrings = {"11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"};
 
-const int64_t part1Expected = -1;
+const int64_t part1Expected = 1227775554;
 const int64_t part2Expected = -1;
 
     
--- day-02/src/part01.cpp
+++ day-02/src/part01.cpp
@@ -2,7 +2,45 @@
  * Day-2 - Part 01
  */
 #include "include.hpp"
+#include <iostream>
+#include <ranges>
+
+using namespace std::ranges;
 
 int64_t handlePart1(const std::vector<std::string> &inputLines) {
-    return 0;
+
+    // Should only have a single line 
+    if( inputLines.size() != 1)
+    {
+        return 0;
+    }
+
+    auto numbers = splitString(inputLines[0], ',');
+    std::cout << "Testing" << std::endl;
+
+    uint64_t total = 0;
+
+    for(const auto& num : numbers)
+    {
+        const auto parsed = splitString(num, '-');
+        uint64_t min = std::stoull(parsed[0]);
+        uint64_t max = std::stoull(parsed[1]);
+        for(const auto curr : iota_view{min, max +1})
+        {
+            const auto strnum = std::to_string(curr);
+            // Only check even length numbers.
+            if((strnum.length() % 2) == 0)
+            {
+                const auto &half1 = strnum.substr(0, strnum.length()/2);
+                const auto &half2 = strnum.substr(strnum.length()/2, strnum.length()/2);
+                if( half1 == half2)
+                {
+                    total += curr;
+                }
+            }
+        }
+    }
+
+
+    return total;
 }
