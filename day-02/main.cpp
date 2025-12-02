847d7aaace00278bf12eb98da5cf6a48c728456e
Aaron Bromma <36652096+bioresestance@users.noreply.github.com>
2025-12-02 19:50:03 +0000

    Day02-part2 done

--- day-02/main.cpp
+++ day-02/main.cpp
@@ -9,7 +9,7 @@
 
 
 #define ENABLE_PART1 (true)
-#define ENABLE_PART2 (false)
+#define ENABLE_PART2 (true)
 
 
 
@@ -17,7 +17,7 @@
 const std::vector<std::string> testInputStrings = {"11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"};
 
 const int64_t part1Expected = 1227775554;
-const int64_t part2Expected = -1;
+const int64_t part2Expected = 4174379265;
 
     
 static std::string getInputFilePath()
--- day-02/src/part02.cpp
+++ day-02/src/part02.cpp
@@ -2,7 +2,67 @@
  * Day-2 - Part 02
  */
 #include "include.hpp"
+#include <iostream>
+#include <ranges>
+
+using namespace std::ranges;
 
 int64_t handlePart2(const std::vector<std::string> &inputLines) {
-    return 0;
+        // Should only have a single line 
+    if( inputLines.size() != 1)
+    {
+        return 0;
+    }
+
+    auto numbers = splitString(inputLines[0], ',');
+    std::cout << "Testing" << std::endl;
+
+    uint64_t total = 0;
+
+    for(const auto& num : numbers)
+    {
+        const auto parsed = splitString(num, '-');
+        uint64_t min = std::stoull(parsed[0]);
+        uint64_t max = std::stoull(parsed[1]);
+        for(const auto curr : iota_view{min, max +1})
+        {
+            const auto strnum = std::to_string(curr);
+
+
+            // We want to split up the string into substring on varying length, up to half the length.
+            for(const auto len : iota_view{1, (int)((strnum.length()/2)+1)})
+            {
+                // Check if the string is divisable evenly, then it wouldnt work
+                if(strnum.length() % len != 0)
+                {
+                    continue;
+                }
+
+                const auto & initialstr = strnum.substr(0, len);
+                bool checked = true;
+                
+                // Now go through all substrings past the first to see if they are all the same.
+                for(uint32_t i = 1; i <= (strnum.length() / len) - 1; i++)
+                {
+                    const auto & newstr = strnum.substr(len*i, len);
+                    if(initialstr != newstr)
+                    {
+                        checked = false;
+                        break;
+                    }
+                }
+                // If all substrings were the same, then it matches the pattern and the number is an invalid ID.
+                if(checked)
+                {
+                    total += curr;
+                    // No point checking the rest of the permutations for the current number.
+                    break;
+                }
+
+            }
+        }
+    }
+
+
+    return total;
 }
