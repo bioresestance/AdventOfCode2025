1f2e8738d0323018dcb611a18f68336df9c6cfca
Aaron Bromma <36652096+bioresestance@users.noreply.github.com>
2025-12-03 19:13:34 +0000

    Add Day 3 template

---
+++ day-03/src/part01.cpp
@@ -0,0 +1,15 @@
+/**
+ * Day-3 - Part 01
+ */
+#include "include.hpp"
+
+int64_t handlePart1(const InputFile &input) {
+
+    auto lines = input.getLines();
+
+    for(const auto& line : lines) {
+        // Process each line
+    }
+
+    return 0;
+}
