29729db84fe10ca34bf4e74544bb4658909dd85d
Aaron Bromma <36652096+bioresestance@users.noreply.github.com>
2025-12-04 16:51:36 +0000

    Day04 - Part2 done

--- day-04/src/part02.cpp
+++ day-04/src/part02.cpp
@@ -3,8 +3,40 @@
  */
 #include "include.hpp"
 
-int64_t handlePart2(const InputFile &input) {
-    const auto &lines = input.getLines();
-    (void)lines;
-    return 0;
+int64_t handlePart2(const InputFile &input)
+{
+    auto map = input.asGrid();
+    uint32_t total = 0;
+
+    uint32_t change = 0;
+    do
+    {
+        change = 0;
+        for (const auto &coord : map.coordinates())
+        {
+
+            if (map[coord] != '@')
+            {
+                continue;
+            }
+
+            const auto &neighbours = map.allNeighbors(coord);
+
+            uint32_t count = 0;
+            for (const auto &cell : neighbours)
+            {
+                if (map[cell] == '@')
+                {
+                    count++;
+                }
+            }
+            if (count < 4)
+            {
+                map[coord] = '.';
+                change++;
+                total++;
+            }
+        }
+    } while (change > 0);
+    return total;
 }
